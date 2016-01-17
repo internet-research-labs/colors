a1da56ced6cce73a047e6ef17ae84c07e01cd765
Matt Owen <not.mattowen@gmail.com>
2016-01-17 18:46:18 -0500

    :wolf:

--- Color.go
+++ Color.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"github.com/disintegration/imaging"
 	"image"
 	"image/color"
 	"math"
 	"math/rand"
 )
+
+func ColorVector(img image.Image) []color.RGBA {
+	scaled_down_image := ScaleDown(img)
+	bounds := scaled_down_image.Bounds()
+	colors := []color.RGBA{}
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			col := img.At(x, y)
+			r, g, b, a := col.RGBA()
+			colors = append(colors, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
+		}
+	}
+
+	return colors
+}
+
+// Returns a List of Centroidsand their Clusters
+func Closest(img image.Image, num_colors int) (color.RGBA, []color.RGBA) {
+	// A+
+	groups := make([][]color.RGBA, num_colors)
+
+	return color.RGBA{}, groups[0]
+}
+
+func ScaleDown(img image.Image) image.Image {
+	return imaging.Resize(img, 200, 200, imaging.Lanczos)
+}
+
+func Kmeans(img image.Image, num_colors int) []color.RGBA {
+	centroids := make([]color.RGBA, 10)
+	for i := 0; i < 100; i++ {
+		_, centroids = Closest(img, num_colors)
+	}
+
+	return centroids
+}
 
 //
 func RandomColor() color.RGBA {
@@ -15,6 +52,15 @@ func RandomColor() color.RGBA {
 		uint8(rand.Intn(255)),
 		255,
 	}
+}
+
+// Images
+func RandomColors(num_colors int) []color.RGBA {
+	palette := make([]color.RGBA, num_colors)
+	for i, _ := range palette {
+		palette[i] = RandomColor()
+	}
+	return palette
 }
 
 // Euclidean Distance
